371b35cd3f5034929595623456c64902043eadf6
gryz <jinlizhong@smail.nju.edu.cn>
2025-03-06 10:22:08 +0800

    could use idx info in editClick

--- frontend/script.js
+++ frontend/script.js
@@ -1,12 +1,51 @@
 console.log("I am running!");
 
+let rowStash = [];
+
+const editClick = (idx) => {
+    return (event) => {
+        if (event.button !== 0) return;
+        /*const btnElem = event.target;
+        const rowElem = btnElem.parentElement.parentElement;
+
+        const rowLength = rowElem.children.length;
+        for (let i = 0; i < rowLength - 1; ++i) {
+            const elem = rowElem.children[i];
+
+            const inputElem = document.createElement("input");
+            inputElem.type = "text";
+            inputElem.value = elem.textContent;
+
+            elem.textContent = "";
+            elem.appendChild(inputElem);
+        }
+
+        const lastTdElem = rowElem.children[rowLength - 1];
+
+        const confirmBtnElem = document.createElement("button");
+        confirmBtnElem.textContent = "确认";
+
+        const cancelBtnElem = document.createElement("button");
+        cancelBtnElem.textContent = "取消";
+
+        btnElem.innerHTML = "";
+        lastTdElem.appendChild(confirmBtnElem);
+        lastTdElem.appendChild(cancelBtnElem);*/
+        console.log(`Row ${idx} was clicked!`);
+        console.log(rowStash.length);
+    }
+};
+
 document.addEventListener("DOMContentLoaded", () => {
     const tableBody = document.getElementById("tableBody");
     fetch("/api/getsheet")
         .then(resp => resp.json())
         .catch(error => console.log(error))
         .then(json => {
-            for (let row of json["data"]) {
+            for (let i = 0; i < json["data"].length; ++i) {
+                let row = json["data"][i];
+
+                rowStash.push(null);
                 const rowElem = document.createElement("tr");
                 tableBody.appendChild(rowElem);
 
@@ -16,12 +55,11 @@ document.addEventListener("DOMContentLoaded", () => {
                     rowElem.appendChild(colElem);
                 }
 
+                const editCell = rowElem.insertCell(-1);
                 const btnElem = document.createElement("button");
                 btnElem.textContent = "编辑";
-                btnElem.onclick = () => {
-                    alert("Hello!");
-                };
-                rowElem.appendChild(btnElem);
+                btnElem.onclick = editClick(i);
+                editCell.appendChild(btnElem);
             }
         });
 });
