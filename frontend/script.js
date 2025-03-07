1e76628dfee70d575af9151fb2c95f7a01f4530b
gryz <jinlizhong@smail.nju.edu.cn>
2025-03-07 14:56:32 +0800

    a little bit wrapper

--- frontend/script.js
+++ frontend/script.js
@@ -3,17 +3,17 @@ console.log("I am running!");
 let rowStash = [];
 let fields = null;
 
-const edit = (idx, btnElem, trElem) => {
+const edit = (idx, trElem) => {
     rowStash[idx] = trElem;
-    trElem.parentElement.replaceChild(createInputRow(trElem, idx), trElem);
+    trElem.parentElement.replaceChild(createEditRowFrom(idx, trElem), trElem);
 };
 
-const cancel = (idx, btnElem, trElem) => {
+const cancel = (idx, trElem) => {
     trElem.parentElement.replaceChild(rowStash[idx], trElem);
     rowStash[idx] = null;
 };
 
-const confirm_writerow = (idx, btnElem, trElem) => {
+const confirm_writerow = (idx, trElem) => {
     trElem.querySelectorAll("input")
           .forEach(input => input.disabled = true);
     trElem.querySelectorAll("button")
@@ -36,7 +36,7 @@ const confirm_writerow = (idx, btnElem, trElem) => {
                 if (i >= fields.length) return;
                 tdElem.textContent = trElem.children[i].firstChild.value;
             });
-            cancel(idx, btnElem, trElem);
+            cancel(idx, trElem);
         } else throw new Error("Network response was not ok");
     })
     .catch(error => {
@@ -48,10 +48,9 @@ const confirm_writerow = (idx, btnElem, trElem) => {
 const rowBtnClick = (idx, action) => {
     return (event) => {
         if (event.button !== 0) return;
-        const btnElem = event.target;
-        const trElem = btnElem.parentElement.parentElement;
+        const trElem = event.target.parentElement.parentElement;
 
-        action(idx, btnElem, trElem);
+        action(idx, trElem);
     };
 }
 
@@ -61,30 +60,33 @@ const createTdWrapper = (elemInside) => {
     return tdElem;
 };
 
-const createInputRow = (normalRow, idx) => {
-    const rowLength = normalRow.children.length;
-    const newRowElem = document.createElement("tr");
-    for (let i = 0; i < rowLength - 1; ++i) {
-        const tdElem = normalRow.children[i];
-
+const createEditRow = (inputcontent, onconfirm, oncancel) => {
+    const rowElem = document.createElement("tr");
+    Array.from({ length: fields.length }, (_, i) => {
         const inputElem = document.createElement("input");
         inputElem.type = "text";
-        inputElem.value = tdElem.textContent;
-
-        newRowElem.appendChild(createTdWrapper(inputElem));
-    }
-
+        inputElem.value = inputcontent[i];
+        rowElem.appendChild(createTdWrapper(inputElem));
+    });
     const confirmBtnElem = document.createElement("button");
     confirmBtnElem.textContent = "确认";
-    confirmBtnElem.onclick = rowBtnClick(idx, confirm_writerow);
+    confirmBtnElem.onclick = onconfirm;
 
     const cancelBtnElem = document.createElement("button");
     cancelBtnElem.textContent = "取消";
-    cancelBtnElem.onclick = rowBtnClick(idx, cancel);
+    cancelBtnElem.onclick = oncancel;
 
-    newRowElem.appendChild(createTdWrapper(confirmBtnElem));
-    newRowElem.appendChild(createTdWrapper(cancelBtnElem));
-    return newRowElem;
+    rowElem.appendChild(createTdWrapper(confirmBtnElem));
+    rowElem.appendChild(createTdWrapper(cancelBtnElem));
+    return rowElem;
+}
+
+const createEditRowFrom = (rowIdx, row) => {
+    return createEditRow(
+        Array.from(row.querySelectorAll("td"), (v, k) => v.textContent),
+        rowBtnClick(rowIdx, confirm_writerow),
+        rowBtnClick(rowIdx, cancel)
+    );
 };
 
 document.addEventListener("DOMContentLoaded", () => {
